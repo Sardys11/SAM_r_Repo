0d912539bcf5a24a2075181705328eb47ac4e741
Sardys11 <142862359+Sardys11@users.noreply.github.com>
2025-03-09 22:05:47 -0400

    Module_8.R

--- Module_8.R
+++ Module_8.R
@@ -1,5 +1,42 @@
 
+# Module # 8 Input/Output, string manipulation and plyr package
+# Student: Sardys Avile-Martinez
 
+# NOTES FROM THE PROFESSOR:
+#   Please following steps 1-3
+# Step # 1 Import assignment 6 Data-set to R Download Import assignment 6 Data-set to R. Then, Run the commend "mean" using Sex as the category (use plyr package for this operation). Last commend in this step: write the resulting output to a file.
+# > <- read.table("<FileName>.txt", header = TRUE)
+# 
+# >install.packages("pryr")
+# require(pryr)
+# require(ISLR)
+# require(boot)
+# install.packages("plyr")
+# library(data.table)
+# library(plyr)
+# etc
+# 
+# #----Read file from computer via prompt
+# Student-assignment-6 <- read.table("<FileName>.txt", header = TRUE)
+# Student-assignment-6
+# StudentAverage = ddply(Student,"Sex",transform,Grade.Average=mean(Grade))
+# 
+# sex = Student$Sex
+# mean(Sex)
+# Step # 2 Convert the data set to a dataframe for names whos' name contains the letter i, then create a new data set with those names, Write those names to a file separated by comma’s (CSV)
+# The commends
+# 
+# >write.table(students_gendered_mean, "Students_Gendered_Mean")
+# 
+# # Filter the original data set to include only data for which the student name
+# # contained the letter i.
+# >i_students <- subset(students, grepl("i", students$Name, ignore.case=T))
+# 
+# Step # 3
+# Write the filtered data set and convert it to CSV file
+# Hint(file.choose() and subset())
+
+############################# My code and comments #####################
 
 #install.packages("pryr")
 # require(pryr)
@@ -65,3 +102,4 @@ i_students <- subset(students, grepl("i", students$Name, ignore.case = TRUE))
 
 head(i_students)
 
+################### the end ################################
